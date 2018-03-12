4729b6471f641a6cf26d0d7d41c79f19ea493c83
Hammad Mushtaq <hkmushtaq@gmail.com>
2018-03-12 19:00:01 -0400

    no message

--- zbar/src/main/java/com/yanzhenjie/zbar/Config.kt
+++ zbar/src/main/java/com/yanzhenjie/zbar/Config.kt
@@ -23,54 +23,54 @@
  *  http://sourceforge.net/projects/zbar
  *------------------------------------------------------------------------*/
 
-package com.yanzhenjie.zbar;
+package com.yanzhenjie.zbar
 
 /**
  * Decoder configuration options.
  */
-public class Config {
+object Config {
     /**
      * Enable symbology/feature.
      */
-    public static final int ENABLE = 0;
+    const val ENABLE = 0
     /**
      * Enable check digit when optional.
      */
-    public static final int ADD_CHECK = 1;
+    const val ADD_CHECK = 1
     /**
      * Return check digit when present.
      */
-    public static final int EMIT_CHECK = 2;
+    const val EMIT_CHECK = 2
     /**
      * Enable full ASCII character set.
      */
-    public static final int ASCII = 3;
+    const val ASCII = 3
 
     /**
      * Minimum data length for valid decode.
      */
-    public static final int MIN_LEN = 0x20;
+    const val MIN_LEN = 0x20
     /**
      * Maximum data length for valid decode.
      */
-    public static final int MAX_LEN = 0x21;
+    const val MAX_LEN = 0x21
 
     /**
      * Required video consistency frames.
      */
-    public static final int UNCERTAINTY = 0x40;
+    const val UNCERTAINTY = 0x40
 
     /**
      * Enable scanner to collect position data.
      */
-    public static final int POSITION = 0x80;
+    const val POSITION = 0x80
 
     /**
      * Image scanner vertical scan density.
      */
-    public static final int X_DENSITY = 0x100;
+    const val X_DENSITY = 0x100
     /**
      * Image scanner horizontal scan density.
      */
-    public static final int Y_DENSITY = 0x101;
+    const val Y_DENSITY = 0x101
 }
